2332e911f186e2191dd085c2ace03cd06f18ff66
xPyThMx <73391218+WinkeyCheng@users.noreply.github.com>
2022-05-02 17:15:29 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+git clone https://github.com/MHProDev/MHDDoS.git
+pip install -r ~/MHDDoS/requirements.txt
