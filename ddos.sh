b17129579509bb96b60446ac2fa88e2e408d344d
xPyThMx <73391218+WinkeyCheng@users.noreply.github.com>
2022-05-02 17:38:17 +0800

    Update ddos.sh

--- ddos.sh
+++ ddos.sh
@@ -1,7 +1,10 @@
 sudo apt update
+sleep 5s
 sudo apt install software-properties-common
+sleep 3s
 sudo add-apt-repository ppa:deadsnakes/ppa
+sleep 3s
 sudo apt update
-sudo apt install python3.8
 git clone https://github.com/MHProDev/MHDDoS.git
+sleep 5s
 pip install -r ~/MHDDoS/requirements.txt
